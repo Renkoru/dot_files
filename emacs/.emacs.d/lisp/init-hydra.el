d15dbc76caac61ec0a1c8501e2f23b0410fd474b
Maxim Rurenko <mrurenko@gmail.com>
2017-08-22 10:57:22 +0600

    [emacs] add multiple cursors hydra

--- emacs/.emacs.d/lisp/init-hydra.el
+++ emacs/.emacs.d/lisp/init-hydra.el
@@ -11,7 +11,18 @@
     ("i" text-scale-increase "increase")
     ("d" text-scale-decrease "decrease"))
 
+  (defhydra hydra-mc (:pre (evil-mc-pause-cursors)
+                      :post (evil-mc-resume-cursors))
+    "multiple cursors"
+    ("l" forward-char)
+    ("h" backward-char)
+    ("j" next-line)
+    ("k" previous-line)
+    ("c" evil-mc-make-cursor-here "create cursor")
+    ("q" nil "quit"))
+
   (evil-leader/set-key "cf" 'hydra-zoom/body)
+  (evil-leader/set-key "mc" 'hydra-mc/body)
 )
 
 (provide 'init-hydra)
