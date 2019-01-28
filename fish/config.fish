0d33721a5c771db7f5ba94558ad9075477178cbc
Maxim Rurenko <mrurenko@gmail.com>
2019-01-28 15:00:10 +0600

    [fish] add abbrevs

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ alias dc="docker-compose"
 alias dps="docker-compose ps"
 alias dlog="docker-compose log"
 
+abbr --add pupd sudo pacman -Suy
+abbr --add yupd yay -Suya
+abbr --add kbd /bin/bash /home/mrurenko/dev_tools/dot_files/scripts/keyboard.sh
+
 set -x SSH_AUTH_SOCK "/run/user/1000/ssh-agent.socket"
 set -x WORKON_HOME $HOME/.local/share/virtualenvs
 
