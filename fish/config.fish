6aff2dddfd8cd88b9911d70b63c7819331a5ee98
Maxim Rurenko <mrurenko@gmail.com>
2019-07-25 20:03:26 +0600

    [fish] update fish and configurations

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,17 @@
-alias dc="docker-compose"
-alias dps="docker-compose ps"
-alias dlog="docker-compose log"
-
 abbr --add lsa ls -la
 abbr --add pupd sudo pacman -Suy
 abbr --add yupd yay -Suya
 abbr --add kbd /bin/bash /home/mrurenko/dev_tools/dot_files/scripts/keyboard.sh
+abbr --add amt xinput set-prop \"rdashevsky work trackpad\" 285 1 and xinput set-prop \"rdashevsky work trackpad\" 296 0.3 and libinput-gestures-setup restart
+
+
+# Docker abbrs
+abbr --add dc docker-compose
+abbr --add dcu docker-compose up -d
+abbr --add dcs docker-compose stop
+abbr --add dcd docker-compose down
+abbr --add dps docker-compose ps
+abbr --add dlog docker-compose log
 
 set -x SSH_AUTH_SOCK "/run/user/1000/ssh-agent.socket"
 set -x WORKON_HOME $HOME/.local/share/virtualenvs
